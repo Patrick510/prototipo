de76997321f6261326c423210f5aa37018a2b280
nicholas <nicholasricardosilvaaraujo@gmail.com>
2024-12-19 13:04:11 -0400

    fazendo prot em flask

---
+++ speechText.py
@@ -0,0 +1,24 @@
+import speech_recognition as sr
+
+recognizer = sr.Recognizer()
+
+def microphone():
+    with sr.Microphone() as source:
+        print("Ajustando para o ruído ambiente... Aguarde um momento.")
+        recognizer.adjust_for_ambient_noise(source)
+        print("Pronto! Pode falar.")
+
+        # Captura o áudio do microfone
+        audio = recognizer.listen(source)
+
+        try:
+            print("Reconhecendo...")
+            text = recognizer.recognize_google(audio, language='pt-BR')
+            print(f"Você disse: {text}")
+
+        except sr.UnknownValueError:
+            print("Desculpe, não entendi o que você disse.")
+        except sr.RequestError as e:
+            print(
+                f"Erro ao solicitar resultados do serviço de reconhecimento de fala; {e}")
+    return text
